5e46031804ad448e9f5883ac3b8b64182acb705f
Benoit Roux <benoit.roux@gmail.com>
2024-05-20 19:14:37 +0000

    chore: Add pandas library and update content in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -14,6 +15,21 @@ with col2:
     software developer. I'm passionate about using data to solve complex problems. I'm currently working on
     image analysis software to automate the cells detection process and tracking over time. I'm also working
     on a high throughput screening software to identify new generation of biosensors. I'm always looking for
-    new challenges and opportunities to learn new things. Feel free to contact me if you have any questions or
-    if you want to collaborate on a project."""
+    new challenges and opportunities to learn new things. """
     st.info(content)
+
+content2 = """Feel free to contact me if you have any questions or
+    if you want to collaborate on a project."""
+st.write(content2)
+
+df = pd.read_csv('data.csv',sep=';')
+titles = df['title']
+col3,col4 = st.columns(2)
+
+with col3:
+    for i in range(0,len(titles),2):
+        st.header(titles[i])
+        
+with col4:
+    for i in range(1,len(titles),2):
+        st.header(titles[i])
