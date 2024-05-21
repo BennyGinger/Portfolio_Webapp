8056e57fbcaa9dbf9f914d1c9ce2238a81ef46a7
Benoit Roux <benoit.roux@gmail.com>
2024-05-21 21:12:02 +0000

    improved side project

--- SidePorject/CompanyWebsite/pages/Contact_Us.py
+++ SidePorject/CompanyWebsite/pages/Contact_Us.py
@@ -1,13 +1,21 @@
 import streamlit as st
+import pandas as pd
+import sys
+sys.path.append('/home/Portfolio_Webapp/')
 from send_email import send_email
 
-st.header("Contact Me")
+st.header("Contact Us!")
+df = pd.read_csv('topics.csv')
 
 with st.form(key='contact_form'):
     user_email = st.text_input(label="Your email address")
+    
+    select_box = st.selectbox(label='What topic do you want to discuss',
+                              options=df.topic)
     raw_message = st.text_area(label='Your message')
     msg = f"""Subject: New message from Portfolio website\n\n
     From: {user_email}\n
+    Topic: {select_box}\n
     {raw_message}"""
     submit_button = st.form_submit_button('Submit')
     
